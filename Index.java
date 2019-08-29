b2b966d45c53cf53bde6131fb29f2d26776a30ae
topetemi285 <topetemi285@gmail.com>
2019-08-29 11:37:29 +0100

    second change

--- Index.java
+++ Index.java
@@ -3,4 +3,5 @@ public class Index
     public static void main(String args[])
     System.out.print("first progra");
     system.out.print("first change");
+    System.out.print("second change");
 }
